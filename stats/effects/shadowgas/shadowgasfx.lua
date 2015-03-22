61e74f7eaa483ad4a0c3085942d4ca553084dae1
sayterdarkwynd <sayterdarkwynd@gmail.com>
2015-03-22 09:26:12 -0400

    87

--- stats/effects/shadowgas/shadowgasfx.lua
+++ stats/effects/shadowgas/shadowgasfx.lua
@@ -1,30 +1,41 @@
 function init()
+  local bounds = mcontroller.boundBox()
   animator.setParticleEmitterOffsetRegion("shadowgaseffect", mcontroller.boundBox())
   animator.setParticleEmitterActive("shadowgaseffect", true)
   effect.setParentDirectives("fade=000000=0.15")
 
   script.setUpdateDelta(5)
-
   self.tickDamagePercentage = 0.01
   self.tickTime = 1.2
   self.tickTimer = self.tickTime
+  
+  
+  self.liquidMovementParameter = {
+    groundForce = 100,
+    airForce = 20,
+    liquidForce = 30,
+    liquidFriction = 0.0,
+    liquidImpedance = 0.5,
+    minimumLiquidPercentage = 0.5,
+    liquidJumpProfile = {
+      jumpSpeed = 30.0,
+      jumpControlForce = 900.0,
+      jumpInitialPercentage = 0.75,
+      jumpHoldTime = 0.2,
+      multiJump = false,
+      reJumpDelay = 0.05,
+      autoJump = false,
+      collisionCancelled = true 
+    }
+  }  
 end
 
-function update(dt)
-mcontroller.controlParameters({
-  liquidFriction = 0.5,
-  liquidImpedance = 0.5,
-    liquidJumpProfile = {
-  jumpSpeed = 30.0,
-  jumpControlForce = 900.0,
-  jumpInitialPercentage = 0.75,
-  jumpHoldTime = 0.2,
-  multiJump = false,
-  reJumpDelay = 0.05,
-  autoJump = false,
-  collisionCancelled = true  }
-  })
 
+
+  
+
+function update(dt)
+mcontroller.controlParameters(self.liquidMovementParameter)
 
   self.tickTimer = self.tickTimer - dt
   if self.tickTimer <= 0 then
