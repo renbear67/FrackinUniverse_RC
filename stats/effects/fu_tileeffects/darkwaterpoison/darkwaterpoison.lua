344c2ad089f1b5800382687f51f5bcddefdcb7c9
sayterdarkwynd <sayterdarkwynd@gmail.com>
2015-05-15 21:22:36 -0400

    additional crystal tree variants for crystalmoons

---
+++ stats/effects/fu_tileeffects/darkwaterpoison/darkwaterpoison.lua
@@ -0,0 +1,29 @@
+function init()
+  animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
+  animator.setParticleEmitterActive("drips", true)
+  
+  script.setUpdateDelta(5)
+
+  self.tickDamagePercentage = 0.055
+  self.tickTime = 0.75
+  self.tickTimer = self.tickTime
+end
+
+function update(dt)
+  self.tickTimer = self.tickTimer - dt
+  if self.tickTimer <= 0 then
+    self.tickTimer = self.tickTime
+    status.applySelfDamageRequest({
+        damageType = "IgnoresDef",
+        damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 1,
+        damageSourceKind = "poison",
+        sourceEntityId = entity.id()
+      })
+  end
+
+  effect.setParentDirectives("fade=AA00AA="..self.tickTimer * 0.4)
+end
+
+function uninit()
+  
+end
