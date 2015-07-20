bc0d0f9402afa014008e4a0321a01c69f3e0ca75
sayterdarkwynd <sayterdarkwynd@gmail.com>
2015-07-19 23:51:24 -0400

    more tiles, faster extraction
    - extraction speed has been increased by x2. Later, each material will
    have varying times.
    - Added several new tiles effects to the newly added tiles. Ice has 3
    levels of severity in terms of slipperiness. All are blocked by the same
    tech as before.
    - More new tiles: Glass Sand, Crystal Sand
    - New Sub-Biome: Crystalline Desert
    - Changed Sub-Biome: Crystal Plains changed “name” to Crystal Volcanic.
    Lava added to it’s liquid, and all others but Organic Soup and Elder
    Liquid removed
    - moons with the “desert” variant have Glass Sand
    - metallic moons given more possible variants
    - crystal moon sub-biomes reduced. Most common now will be Crystal
    Swamp, Crystal Desert and Crystal Volcanic
    - new tile extraction recipes altered to be a bit less rewarding and
    more in-line with similar tiles.
    - all new sand tiles now smelt to glass or extract to silicon

--- scripts/fu_tilegroundeffects.lua
+++ scripts/fu_tilegroundeffects.lua
@@ -11,14 +11,19 @@ function tileMaterials()
   self.matCheck["mud"] = {{"fumudslow"},0,0,0,14,100,0,0.75}
   self.matCheck["clay"] = {{"fumudslow"},0,0,0,14,100,0,0.75}
   self.matCheck["sand"] = {{"jungleslow"},0,0,0,14,100,0,0.9}
+  self.matCheck["rainbowsand"] = {{"jungleslow"},0,0,0,14,100,0,0.2}
+  self.matCheck["glowingsand"] = {{"glow"},0,0,0,14,100,0,0.2} 
+  self.matCheck["crystalsand"] = {{"jungleslow"},0,0,0,14,100,0,0.9}
+  self.matCheck["glasssand"] = {{"jungleslow"},0,0,0,14,100,0,0.9}
   self.matCheck["redsand"] = {{"jungleslow"},0,0,0,14,100,0,0.9}
   self.matCheck["jungledirt2"] = {{"jungleslow"},0,0,0,14,100,0,1}
   self.matCheck["swampdirtff"] = {{"jungleslow"},0,0,0,14,100,0,1}
   self.matCheck["frozenwater"] = {{"iceslip"},0,0,0,14,100,0,1}
   self.matCheck["ice"] = {{"iceslip"},0,0,0,14,100,0,1}
-  --self.matCheck["iceblock"] = {{"iceslip"},0,0,0,14,100,0,1}
   self.matCheck["iceblock1"] = {{"iceslip"},0,0,0,14,100,0,1}
   self.matCheck["iceblock2"] = {{"iceslip"},0,0,0,14,100,0,1}
+  self.matCheck["iceblock3"] = {{"iceslip3"},0,0,0,14,100,0,0.2}
+  self.matCheck["iceblock4"] = {{"iceslip2"},0,0,0,14,100,0,0.2}
   self.matCheck["snow"] = {{"snowslow"},0,0,0,14,100,0,1}
   self.matCheck["slush"] = {{"slushslow"},0,0,0,14,100,0,1}
   self.matCheck["slime"] = {{"slimestick"},0,0,0,14,100,0,0.75}
@@ -38,4 +43,6 @@ function tileMaterials()
   self.matCheck["asphalt"] = {{"metalspeed"},0,0,0,14,100,0,1.4}
   self.matCheck["cloudblock"] = {{"lowgrav"},0,0,0,14,100,0,0.2}
   self.matCheck["raincloud"] = {{"lowgrav"},0,0,0,14,100,0,0.2}
+
+          
 end
