d85e4bea56683e7b268e2e109e994b94d2e78e70
sayterdarkwynd <sayterdarkwynd@gmail.com>
2015-10-29 15:30:51 -0400

    new armor, weapons and gear
    also corrected vanity head masks

--- stats/effects/jumpboost/jumpboost35.lua
+++ stats/effects/jumpboost/jumpboost35.lua
@@ -6,7 +6,7 @@ end
 function update(dt)
   animator.setParticleEmitterActive("jumpparticles", mcontroller.jumping())
   mcontroller.controlModifiers({
-      jumpModifier = 0.55
+      jumpModifier = 0.35
     })
 end
 
