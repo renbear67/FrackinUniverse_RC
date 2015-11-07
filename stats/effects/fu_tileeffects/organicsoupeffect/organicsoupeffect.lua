c6caf5a76cc1fc003bf8ee5c6a8c90de40c2b5e6
sayterdarkwynd <sayterdarkwynd@gmail.com>
2015-11-07 08:50:14 -0500

    dungeon goodness

--- stats/effects/fu_tileeffects/organicsoupeffect/organicsoupeffect.lua
+++ stats/effects/fu_tileeffects/organicsoupeffect/organicsoupeffect.lua
@@ -1,6 +1,6 @@
 function init()
   _x = effect.configParameter("healthDown", 0)
-baseValue = effect.configParameter("healthDown",0)*(status.resourceMax("energy"))
+baseValue = effect.configParameter("healthDown",0)*(status.resourceMax("health"))
 
   if (status.resourceMax("health")) * _x >= 100.0 then
      effect.addStatModifierGroup({{stat = "maxHealth", amount = baseValue }})
@@ -8,37 +8,23 @@ baseValue = effect.configParameter("healthDown",0)*(status.resourceMax("energy")
      effect.addStatModifierGroup({{stat = "maxHealth", amount = baseValue }})
   end
   
-  effect.setParentDirectives("border=2;0088FF99;00000000")
+  effect.setParentDirectives("border=1;aa00aa00;00000000")
   
   script.setUpdateDelta(5)
 
   self.tickDamagePercentage = 0.0
   self.tickTime = 3.0
   self.tickTimer = self.tickTime  
-  activateVisualEffects()
 end
 
-function activateVisualEffects()
-  animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
-  animator.setParticleEmitterActive("drips", true)
-  local statusTextRegion = { 0, 1, 0, 1 }
-  animator.setParticleEmitterOffsetRegion("statustext", statusTextRegion)
-  animator.burstParticleEmitter("statustext")
-end
 
 function update(dt)
   self.tickTimer = self.tickTimer - dt
   if self.tickTimer <= 0 then
     self.tickTimer = self.tickTime
-    status.applySelfDamageRequest({
-        damageType = "IgnoresDef",
-        damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 1,
-        damageSourceKind = "poison",
-        sourceEntityId = entity.id()
-      })
   end
 
-  effect.setParentDirectives("fade=EEEEEE="..self.tickTimer * 0.4)
+  effect.setParentDirectives("fade=aa00aa="..self.tickTimer * 0.4)
 
 end
 
