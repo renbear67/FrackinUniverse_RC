b8e6190ab0a46b7730e44f2097ffad48e260e905
sayterdarkwynd <sayterdarkwynd@gmail.com>
2015-11-15 00:07:15 -0500

    fixes and adjustments

--- stats/effects/fu_weathereffects/ffextremesulphuric/ffextremesulphuric.lua
+++ stats/effects/fu_weathereffects/ffextremesulphuric/ffextremesulphuric.lua
@@ -1,29 +1,34 @@
 function init()
   animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
   animator.setParticleEmitterActive("drips", true)
-  
+  effect.setParentDirectives("fade=8a6818=0.2")
   script.setUpdateDelta(5)
-
-  self.tickDamagePercentage = 0.04
+  self.tickDamagePercentage = 0.03
   self.tickTime = 1.0
   self.tickTimer = self.tickTime
 end
 
+
+
 function update(dt)
+  mcontroller.controlModifiers({
+      groundMovementModifier = -0.15,
+      runModifier = -0.15,
+      jumpModifier = -0.15
+    })
+    
   self.tickTimer = self.tickTimer - dt
   if self.tickTimer <= 0 then
     self.tickTimer = self.tickTime
     status.applySelfDamageRequest({
         damageType = "IgnoresDef",
-        damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 1,
+        damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 2,
         damageSourceKind = "poison",
         sourceEntityId = entity.id()
       })
   end
-
-  effect.setParentDirectives("fade=8a6818="..self.tickTimer * 0.4)
 end
 
 function uninit()
-  
+
 end
