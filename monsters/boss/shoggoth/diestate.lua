fdb92c47cd42108a229d9e168b68697054b75ef6
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-01-03 00:46:28 -0500

    shoggoth updates, glarestalk growth fix

--- monsters/boss/shoggoth/diestate.lua
+++ monsters/boss/shoggoth/diestate.lua
@@ -1,17 +1,25 @@
+--------------------------------------------------------------------------------
 dieState = {}
 
-function dieState.enterWith(args)
-  if not args.die then return nil end
+dieState.enterWith = function(params)
+  if not params.die then return nil end
+  
+  rangedAttack.setConfig(entity.configParameter("projectiles.deathexplosion.type"), entity.configParameter("projectiles.deathexplosion.config"), 0.2)
 
   return {
-    timer = 1.0
+    timer = 6.0,
+    basePosition = mcontroller.position()
   }
 end
 
 function dieState.enteringState(stateData)
+  entity.setAnimationState("movement", "death")
 end
 
-function dieState.update(dt, stateData)
+dieState.update = function(dt, stateData)
+  mcontroller.controlParameters({gravityEnabled = true})
+
+  
   if stateData.timer <= 0.0 then
     self.dead = true
   end
--- monsters/boss/shoggoth/skills/eyebeamattack.lua
+++ monsters/boss/shoggoth/skills/eyebeamattack.lua
@@ -20,17 +20,19 @@ end
 --------------------------------------------------------------------------------
 function eyeBeamAttack.enteringState(stateData)
   entity.setAnimationState("movement", "idle")
-  entity.setAnimationState("firstBeams", "windup")
   entity.setActiveSkillName("eyeBeamAttack")
 end
 
 --------------------------------------------------------------------------------
 function eyeBeamAttack.update(dt, stateData)
   if not hasTarget() then return true end
-
-  local toTarget = world.distance(self.targetPosition, vec2.add(mcontroller.position(), {-10, -5}))
+  local toTarget = world.distance(self.targetPosition, mcontroller.position())
   local targetDir = util.toDirection(toTarget[1])
-
+  if (targetDir == 1) then 
+    toTargetAim = world.distance(self.targetPosition, vec2.add(mcontroller.position(), {10, -5}))
+  else
+    toTargetAim = world.distance(self.targetPosition, vec2.add(mcontroller.position(), {-10, -5}))
+  end
 
   if not stateData.blasting then 
     if math.abs(toTarget[1]) > stateData.distanceRange[2] then
@@ -57,18 +59,20 @@ function eyeBeamAttack.update(dt, stateData)
       end
       stateData.windupTimer = stateData.windupTimer - dt
       if stateData.windupTimer  < 0 then
+          world.logInfo("Help, I'm stuck in windup.")
           entity.setLightActive("beam1", true)
           entity.setAnimationState("firstBeams", "active")
           
           -- rotate the eyebeam animation to aim at user
-          local animationAngle = math.atan(-toTarget[2], math.abs(toTarget[1]))
+          local animationAngle = math.atan(-toTargetAim[2], math.abs(toTargetAim[1]))
           entity.rotateGroup("projectileAim", animationAngle)
-          entity.targetSnapshot = toTarget
+          entity.targetSnapshot = toTargetAim
 
       end
       return flase
     -- phase 2 - active (????)
     elseif stateData.timer > 0 then
+      world.logInfo("Help I'm stuck blasting")
       eyeBeamAttack.blast(entity.targetSnapshot)
       stateData.timer = stateData.timer - dt
       if stateData.timer < 0 then
