5834454dcd8548568b96312e4d4a0d656b2fa5ba
sayterdarkwynd <sayterdarkwynd@gmail.com>
2016-02-19 15:58:52 -0500

    bop. armor rebalances

--- stats/effects/fu_armoreffects/exosuithead.lua
+++ stats/effects/fu_armoreffects/exosuithead.lua
@@ -2,7 +2,7 @@ function init()
   script.setUpdateDelta(5)
   --- energyRegenPercentageRate base = 0.285
   --- energyRegenBlockDischarge base = -1.0
-  effect.addStatModifierGroup({{stat = "energyRegenPercentageRate", amount = 0.05}})
+  effect.addStatModifierGroup({{stat = "energyRegenPercentageRate", amount = 1.05}})
   effect.addStatModifierGroup({{stat = "energyRegenBlockDischarge", amount = -2}})
 end
 
